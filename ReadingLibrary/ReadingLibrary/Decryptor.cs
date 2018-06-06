0624e416b54cb31492e969e780533389838ca2ff
soat <school3@soat.fr>
2018-06-06 11:37:36 +0200

    Change the implementation of the encryption algorithm and read json file encrypt

---
+++ ReadingLibrary/ReadingLibrary/Decryptor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ReadingLibrary
+{
+    public static class Decryptor
+    {
+        private static readonly byte[] _key = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+        private static readonly byte[] _iv = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+
+        private static readonly ICryptoTransform _decryptor;
+
+        static Decryptor()
+        {
+            var myRijndael = new RijndaelManaged { Key = _key, IV = _iv, Padding = PaddingMode.PKCS7 };
+            _decryptor = myRijndael.CreateDecryptor(myRijndael.Key, myRijndael.IV);
+        }
+
+        public static string ReadEncryptFile(string input)
+        {
+            using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(input)))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, _decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }}
--- ReadingLibrary/ReadingLibrary/ReadingFile.cs
+++ ReadingLibrary/ReadingLibrary/ReadingFile.cs
@@ -55,75 +55,17 @@ namespace ReadingLibrary
 
         }
 
-        public string ReadEncryptedTxtFile(String FileName, byte[] Key, byte[] vector)
+        public string ReadEncryptedTxtFile(String fileName)
         {
-            //Decrypts a file using Rijndael algorithm.
 
-            try
-            {
+            return Decryptor.ReadEncryptFile(fileName);
 
-                FileStream fStream = File.Open(FileName, FileMode.OpenOrCreate);
-
-                Rijndael RijndaelAlg = Rijndael.Create();
-
-                CryptoStream cStream = new CryptoStream(fStream,
-                    RijndaelAlg.CreateDecryptor(Key, vector),
-                    CryptoStreamMode.Read);
-
-                StreamReader sReader = new StreamReader(cStream);
-
-                string val = null;
-
-                try
-                {
-
-                    val = sReader.ReadLine();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("An error occurred: {0}", e.Message);
-                }
-                finally
-                {
-                    sReader.Close();
-                    sReader.Dispose();
-                    cStream.Close();
-                    cStream.Dispose();
-                    fStream.Close();
-                    fStream.Dispose();
-                }
-
-
-                return val;
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
-                return null;
-            }
-            catch (UnauthorizedAccessException e)
-            {
-                Console.WriteLine("A file error occurred: {0}", e.Message);
-                return null;
-            }
         }
 
-        public DataSet ReadEncryptedXmlFile(string fileName)
+        public string  ReadEncryptedXmlFile(string fileName)
         {
 
-            DataSet ds = new DataSet();
-            FileStream aFileStream = new FileStream(fileName, FileMode.Open);
-            StreamReader aStreamReader = new StreamReader(aFileStream);
-            UnicodeEncoding aUE = new UnicodeEncoding();
-            byte[] key = aUE.GetBytes("password");
-            RijndaelManaged RMCrypto = new RijndaelManaged();
-            CryptoStream aCryptoStream = new CryptoStream(aFileStream, RMCrypto.CreateDecryptor(key, key), CryptoStreamMode.Read);
-
-            ds.ReadXml(aCryptoStream);
-            aStreamReader.Close();
-            aFileStream.Close();
-            return ds;
-            
+            return Decryptor.ReadEncryptFile(fileName);
         }
 
 
@@ -135,8 +77,12 @@ namespace ReadingLibrary
             {
                return (JObject)JToken.ReadFrom(reader);
             }
-
            
+        }
+
+        public string  ReadEncryptedJsonFile(string fileName)
+        {
+            return  Decryptor.ReadEncryptFile(fileName);
         }
     }
 }
